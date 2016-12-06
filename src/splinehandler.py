f22a25c2a8e770ef907f17944586a3ec1c9795b1
Kenyon Prater <kenyonprater3.14@gmail.com>
2016-12-06 11:12:01 -0800

    Completely start over on splines. Start with points

--- src/splinehandler.py
+++ src/splinehandler.py
@@ -1,99 +1,96 @@
-
 import numpy as np
 
-class Path():
-    """A thing that defines a path"""
-    def __init__(self):
-        pass
+class Point():
+    def __init__(self, array):
+        self._arr = np.array(array)
 
-    def sample(self, t):
-        """
-        Returns the value of the Path at t in [0,1]
-        """
-        return np.array([0])
+    def _setElement(self, index, val):
+        self._arr[index] = val
 
-    def getDimension(self):
-        """
-        Returns the length of the vector returned by a call to Path.sample()
-        """
-        return 0
+    def _getElement(self, index):
+        return self._arr[index]
 
-    def toArr(self, n):
-        """
-        Returns a numpy array of n values of the Path evenly spaced from 0 to 1
-        """
-        a = np.arange(n)
-        vals = a / (n-1)
-        arr = np.zeros((n,self.getDimension()))
-        for i in range(n):
-            arr[i] = self.sample(vals[i])
-        return arr
+    def __add__(self, other):
+        return Point(self._arr + other._arr)
 
-class Bezier(Path):
-    """A bezier curve in arbitrary dimensions"""
-    def __init__(self, points):
-        """
-        Create a new bezier curve from points
-        @param points - a 2d numpy array. dimension 1 is seperate points,
-                        dimenison 2 is the coordinates in different dimensions for each point.
-        """
-        super().__init__()
-        self._points = np.array(points, dtype='float64')
+    def __sub__(self, other):
+        return Point(self._arr - other._arr)
 
-    def addPoint(self, point):
-        self._points = np.append(self._points, np.array([point]), axis=0)
-        print(self._points)
+    def __str__(self):
+        return "Point: " + str(self._arr)
 
-    def insertPoint(self, point, index=0):
-        self._points = np.insert(self._points, index, point, axis=0)
+    def __truediv__(self, number):
+        return Point(self._arr/2)
 
-    def getDimension(self):
-        """
-        Returns the length of the vector returned by a call to Path.sample()
-        """
-        return self._points.shape[1]
+class DrawingPoint(Point):
+    def __init__(self, pos, color, size, hardness):
+        self._arr = np.array(pos + color + (size,) + (hardness,), dtype='float64')
 
-    def sample(self, t):
-        """For a given t in [0,1], find the coordinates of the Bezier curve at that t"""
-        arr = self._points
-        while(arr.shape[0] > 1):
-            newarr = np.zeros((arr.shape[0]-1, arr.shape[1]))
-            for i in range(arr.shape[0]-1):
-                newarr[i] = lerp(arr[i],arr[i+1], t)
-            arr=newarr
-        return arr
+    def setX(self, x):
+        self._arr[0] = x
 
-class ControlPoint():
-    def __init__(self, center, handle1, handle2 = None):
-        self._center = center
-        self._handle1 = handle1
-        self._handle2 = handle2 if handle2 else handle1 # If handle2 specified, sharp corner
+    def setY(self, y):
+        self._arr[1] = y
 
-    def makeBezier(self, next):
-        return Bezier(np.array((self._center, self._handle2, next._handle1, next._center)))
+    def setPos(self, pos):
+        self._arr[0:2] = pos
 
-class CompositeBezier(Bezier):
-    def __init__(self, control_points):
-        self._control_points = control_points
-        self._beziers = []
-        self.generateBeziers()
+    def setRed(self, r):
+        self._arr[2] = r
 
-    def generateBeziers(self):
-        self._beziers = []
-        for i in range(len(self._control_points)-1):
-            self._beziers += [self._control_points[i].makeBezier(self._control_points[i+1])]
-        self._maxT = len(self._beziers)
+    def setGreen(self, g):
+        self._arr[3] = g
 
-    def sample(self, t):
-        choice = int(t-1) if t==self.getMaxT() else int(t)
-        t = 1 if t ==self.getMaxT() else t%1
-        return self._beziers[choice].sample(t)
+    def setBlue(self, b):
+        self._arr[4] = b
 
+    def setAlpha(self, a):
+        self._arr[5] = a
 
-def lerp(a, b, t):
-    return b*t + a*(1-t)
+    def setColor(self, color):
+        self._arr[2:6] = pos
+
+    def setSize(self, size):
+        self._arr[6] = size
+
+    def setHardness(self, hardness):
+        self._arr[7] = hardness
+
+    def getX(self):
+        return self._arr[0]
+
+    def getY(self):
+        return self._arr[1]
+
+    def getPos(self):
+        return tuple(self._arr[0:2])
+
+    def getRed(self):
+        return self._arr[2]
+
+    def getGreen(self):
+        return self._arr[3]
+
+    def getBlue(self):
+        return self._arr[4]
+
+    def getAlpha(self):
+        return self._arr[5]
+
+    def getColor(self):
+        return tuple(self._arr[2:6])
+
+    def getSize(self):
+        return self._arr[6]
+
+    def getHardness(self):
+        return self._arr[7]
+
+    def __str__(self):
+        return "DrawingPoint(x:{}, y:{}, rgba:{}, size:{}, hardness:{})".format(self.getX(), self.getY(), self.getColor(), self.getSize(), self.getHardness())
 
 if __name__ == '__main__':
-    a = Bezier(np.array([[0,0],[1,0]]))
-    a.addPoint([.5,1])
-    print(a.toArr(20))
+    a = DrawingPoint((1,2), (255, 0, 0, 0), 10, .75)
+    b = DrawingPoint((4,3), (0, 0, 255, 0), 10, .75)
+    print(a)
+    print((a+b)/2)
